db41d1b7c41681c663aee82783af30f9cd682305
susko8 <samuel.susoliak@gmail.com>
2018-01-25 10:43:37 +0100

    Vylepsena logika nastavenia Intervalov
    Uz nie je mozne nastavit nezobrazitelne Intervaly

--- app/src/main/java/com/samuel/altzasuvkaapp/fragments/ConfigFragment.java
+++ app/src/main/java/com/samuel/altzasuvkaapp/fragments/ConfigFragment.java
@@ -1,5 +1,6 @@
 package com.samuel.altzasuvkaapp.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.NumberPicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.gson.Gson;
 import com.samuel.altzasuvkaapp.Intervals;
 import com.samuel.altzasuvkaapp.R;
@@ -89,6 +92,8 @@ public class ConfigFragment extends Fragment
     }
     public void setPickers(View rootView)
     {
+        Context context = getActivity().getApplicationContext();
+        final Toast toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
         CheapTo= (NumberPicker) rootView.findViewById(R.id.cheap_picker_to);
         CheapFrom=(NumberPicker) rootView.findViewById(R.id.cheap_picker_from);
         ExpTo=(NumberPicker) rootView.findViewById(R.id.exp_picker_to);
@@ -111,37 +116,80 @@ public class ConfigFragment extends Fragment
         ExpTo.setValue(intervaly.getExpTo());
         CheapFrom.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-        intervaly.setCheapFrom(newVal);
-                saveSettings();
+        public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+            {
+                if(newVal < intervaly.getCheapTo())
+                {
+                    intervaly.setCheapFrom(newVal);
+                    saveSettings();
+                    toast.setText("Value set successfully");
+                    toast.show();
+                }
+                else
+                {
+                    toast.setText("Cannot set this value");
+                    toast.show();
+                }
     }
     });
         CheapTo.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
     @Override
-    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-        intervaly.setCheapTo(newVal);
-        saveSettings();
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+    {
+        if(newVal > intervaly.getCheapFrom()) {
+            intervaly.setCheapTo(newVal);
+            saveSettings();
+            toast.setText("Value set successfully");
+            toast.show();
+        }
+        else
+        {
+           toast.setText("Cannot set this value");
+           toast.show();
+
+        }
     }
 });
         ExpFrom.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                intervaly.setExpFrom(newVal);
-                saveSettings();
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+            {
+                if(newVal < intervaly.getExpTo())
+                {
+                    intervaly.setExpFrom(newVal);
+                    saveSettings();
+                    toast.setText("Value set successfully");
+                    toast.show();
+                }
+                else
+                    {
+                        toast.setText("Cannot set this value");
+                        toast.show();
+                }
             }
         });
         ExpTo.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                intervaly.setExpTo(newVal);
-                saveSettings();
+                if(newVal > intervaly.getExpFrom())
+                {
+                    intervaly.setExpTo(newVal);
+                    saveSettings();
+                    toast.setText("Value set successfully");
+                    toast.show();
+                }
+                else
+                {
+                    toast.setText("Cannot set this value");
+                    toast.show();
+                }
             }
         });
 
     }
     public void saveSettings()
     {
-        SharedPreferences sharedPref = getActivity().getSharedPreferences("settings", Context.MODE_APPEND);
+        @SuppressLint("WrongConstant") SharedPreferences sharedPref = getActivity().getSharedPreferences("settings", Context.MODE_APPEND);
         SharedPreferences.Editor editor = sharedPref.edit();
         Gson gson = new Gson();
         String json = gson.toJson(intervaly);
@@ -150,7 +198,7 @@ public class ConfigFragment extends Fragment
     }
     public void RetrieveSettings()
     {
-        SharedPreferences sharedPref = getActivity().getSharedPreferences("settings", Context.MODE_APPEND);
+        @SuppressLint("WrongConstant") SharedPreferences sharedPref = getActivity().getSharedPreferences("settings", Context.MODE_APPEND);
         if(sharedPref.contains("Intervaly"))
         {
             Gson gson = new Gson();
